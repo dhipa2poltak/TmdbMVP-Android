7419b4390ebe41b021471ec710bb9fc4e95ff884
Dhipa Poltak <dhipa2poltak@gmail.com>
2022-05-24 01:32:06 +0700

    enabling StrictMode

--- app/src/main/java/com/dpfht/tmdbmvp/TheApplication.kt
+++ app/src/main/java/com/dpfht/tmdbmvp/TheApplication.kt
@@ -2,11 +2,15 @@ package com.dpfht.tmdbmvp
 
 import android.app.Application
 import android.content.Context
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy.Builder
+import android.os.StrictMode.VmPolicy
 import androidx.multidex.MultiDex
 import com.dpfht.tmdbmvp.di.ApplicationComponent
 import com.dpfht.tmdbmvp.di.ApplicationModule
 import com.dpfht.tmdbmvp.di.DaggerApplicationComponent
 import com.dpfht.tmdbmvp.di.NetworkModule
+
 
 class TheApplication: Application() {
 
@@ -15,6 +19,19 @@ class TheApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder().detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
 
         applicationComponent = DaggerApplicationComponent
             .builder()
