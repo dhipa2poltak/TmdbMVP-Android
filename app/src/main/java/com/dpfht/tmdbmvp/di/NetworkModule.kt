072f29c5e38fdd3afc806acc9b98fb63d3e26538
Dhipa Poltak <dhipa2poltak@gmail.com>
2022-06-06 19:55:09 +0700

    added UnsafeOkHttpClient for running on Debug mode

--- app/src/main/java/com/dpfht/tmdbmvp/di/NetworkModule.kt
+++ app/src/main/java/com/dpfht/tmdbmvp/di/NetworkModule.kt
@@ -2,14 +2,13 @@ package com.dpfht.tmdbmvp.di
 
 import com.dpfht.tmdbmvp.BuildConfig
 import com.dpfht.tmdbmvp.Config
+import com.dpfht.tmdbmvp.data.api.AuthInterceptor
 import com.dpfht.tmdbmvp.data.api.RestService
+import com.dpfht.tmdbmvp.data.api.UnsafeOkHttpClient
 import dagger.Module
 import dagger.Provides
 import okhttp3.CertificatePinner
-import okhttp3.Interceptor
-import okhttp3.Interceptor.Chain
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,8 +19,37 @@ class NetworkModule {
 
   @Singleton
   @Provides
-  fun provideRestService(retrofit: Retrofit): RestService {
-    return retrofit.create(RestService::class.java)
+  fun provideCertificatePinner(): CertificatePinner {
+    return CertificatePinner.Builder()
+      .add("api.themoviedb.org", "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+      .add("api.themoviedb.org", "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+      .add("api.themoviedb.org", "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+      .add("api.themoviedb.org", "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+      .build()
+  }
+
+  @Provides
+  @Singleton
+  fun httpLoggingInterceptor(): HttpLoggingInterceptor {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    return httpLoggingInterceptor
+  }
+
+  @Provides
+  @Singleton
+  fun provideClient(certificatePinner: CertificatePinner): OkHttpClient {
+    if (BuildConfig.DEBUG) {
+      return UnsafeOkHttpClient.getUnsafeOkHttpClient()
+    }
+
+    val httpClientBuilder = OkHttpClient()
+      .newBuilder()
+      .certificatePinner(certificatePinner)
+
+    httpClientBuilder.addInterceptor(AuthInterceptor())
+
+    return httpClientBuilder.build()
   }
 
   @Singleton
@@ -36,56 +64,7 @@ class NetworkModule {
 
   @Singleton
   @Provides
-  fun provideClient(
-    certificatePinner: CertificatePinner,
-    httpLoggingInterceptor: HttpLoggingInterceptor
-  ): OkHttpClient {
-    val httpClientBuilder = OkHttpClient()
-      .newBuilder()
-      .certificatePinner(certificatePinner)
-
-    if (!httpClientBuilder.interceptors().contains(httpLoggingInterceptor) && BuildConfig.DEBUG) {
-      httpClientBuilder.addInterceptor(httpLoggingInterceptor)
-    }
-
-    httpClientBuilder.addInterceptor(object : Interceptor {
-      override fun intercept(chain: Chain): Response {
-        val original = chain.request()
-        val originalHttpUrl = original.url
-
-        val url = originalHttpUrl.newBuilder()
-          .addQueryParameter("api_key", Config.API_KEY)
-          .build()
-
-        val requestBuilder = original.newBuilder()
-          .url(url)
-          .method(original.method, original.body)
-
-        val request = requestBuilder.build()
-
-        return chain.proceed(request)
-      }
-    })
-
-    return httpClientBuilder.build()
-  }
-
-  @Singleton
-  @Provides
-  fun provideCertificatePinner(): CertificatePinner {
-    return CertificatePinner.Builder()
-      .add("api.themoviedb.org", "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
-      .add("api.themoviedb.org", "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
-      .add("api.themoviedb.org", "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
-      .add("api.themoviedb.org", "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
-      .build()
-  }
-
-  @Singleton
-  @Provides
-  fun httpLoggingInterceptor(): HttpLoggingInterceptor {
-    val httpLoggingInterceptor = HttpLoggingInterceptor()
-    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-    return httpLoggingInterceptor
+  fun provideRestService(retrofit: Retrofit): RestService {
+    return retrofit.create(RestService::class.java)
   }
 }
